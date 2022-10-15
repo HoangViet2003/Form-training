28c78bcb8579a9b23d30f8409222cfff82fc71b1
HoangViet2003 <nguyenhoangviet10d1@gmail.com>
2022-10-15 15:42:07 +0700

    edit file

--- formtraining/src/App.js
+++ formtraining/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Formilk from "./Formilk";
 
 function App() {
   return (
     <div className="App">
-     sf
+      <Formilk />
     </div>
   );
 }
---
+++ formtraining/src/Formilk.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Formilk() {
+  return (
+    <div>
+      <form>
+        <input type="text"/>
+        <input type="text"/>
+        <input type="text"/>
+      </form>
+    </div>
+  );
+}
+
+export default Formilk;
